241fb300196a377b67d88f26dac7ddfb9aadb6c3
AmitSherman1 <104097658+AmitSherman1@users.noreply.github.com>
2023-08-17 10:28:11 +0300

    game- basic structure

---
+++ NyanCat-main/Assets/PlayerMovment.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    private enum MovmementState { walking, flying, catnip, turbo, hurt, rainbow}
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
